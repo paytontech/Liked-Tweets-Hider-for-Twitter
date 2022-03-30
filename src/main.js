e8540a9ea8289b1f9325ba41e2be2713df27d6cc
Alex Bastola <bastola.alex.2008@gmail.com>
2022-03-30 12:45:58 -0400

    added event listener

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-submit.addEventListener("click", validate);
+document.addEventListener('keydown', validate)
 
-function validate() {
-    
+function validate(e) {
+    if (e.code !== "Enter") {return 1}
+    console.log(document.getElementById("Username"))
 }
