fc7f113fba71486407d1521cfe608dd0d65a376f
Alex Bastola <bastola.alex.2008@gmail.com>
2022-03-31 16:34:18 -0400

    added if enter key

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-document.addEventListener('keydown', getUsername)
+document.addEventListener('keyup', getUsername)
 
 function getUsername(e) {
+    if(e.keycode == 13){
     const username = Username.value
     console.log(username)
+    }
 }
