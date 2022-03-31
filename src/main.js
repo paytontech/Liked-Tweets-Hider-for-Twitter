5185ed163ad6220453e544ec589ef2bbde3c9397
Alex Bastola <bastola.alex.2008@gmail.com>
2022-03-31 16:24:06 -0400

    removed while loop

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 document.addEventListener('keydown', getUsername)
 
 function getUsername(e) {
-    while(e.value !== "Enter"){
-        const username = Username.value
-        console.log(username)
-    }
+    const username = Username.value
+    console.log(username)
 }
