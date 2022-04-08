7d2209fdde63dcb8f10b4338f8653c26777c074e
Alex Bastola <bastola.alex.2008@gmail.com>
2022-04-08 19:51:09 -0400

    .

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-document.addEventListener('click', getUsername);
+//validate username 
 
-function getUsername(e) {
-    const username = Username.value
-    return username
-}
+// if valid get ID from username | if not valid then let user know
+
+// use ID to get 100 most recent liked tweets
+
+// match them against the feed and remove them via DOM
