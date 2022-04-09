72ca545f3131ee0ec2107283119964a48fdf0922
Alex-Bastola <102440633+Alex-Bastola@users.noreply.github.com>
2022-04-08 21:15:00 -0400

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-//validate username 
-
-// if valid get ID from username | if not valid then let user know
-
-// use ID to get 100 most recent liked tweets
-
-// match them against the feed and remove them via DOM
