ad5a75b4eea6157ad17d130f9fb00df4bc4d1529
Alex Bastola <bastola.alex.2008@gmail.com>
2022-04-08 20:12:44 -0400

    l

--- src/main.js
+++ src/main.js
@@ -4,3 +4,12 @@ function getUsername(e) {
     const username = Username.value
     return username
 }
+
+/*TODO: 
+
+Convert Username to ID
+Use ID to get latest Liked tweets
+send this info to a backround.js (somehow)
+remove the tweet with the ID from API using the DOM
+
+*/
