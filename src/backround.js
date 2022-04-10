5c70bf8385b9c2d4ff37001d5007fafbf3f4e103
Alex Bastola <bastola.alex.2008@gmail.com>
2022-04-10 10:54:25 -0400

    added -broken- chrome messages

--- src/backround.js
+++ src/backround.js
@@ -1,1 +1,6 @@
-
+chrome.runtime.onMessage.addListener(
+    function(request) {
+      let username = request.greeting
+      return username
+    }
+)
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("keydown", function(event) {
     if (event.code === 'Enter') {
         let username = document.getElementById("Username").value 
+        chrome.runtime.sendMessage({greeting: username})
     }
 });
